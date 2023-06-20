666692f09008f5fe597714e02d339eafac186227
Shayak Roychowdhury <69388919+Shayak99@users.noreply.github.com>
2023-06-20 16:37:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import OtpInput from "./OtpInput";
+
+export default function App() {
+  return (
+    <div className="App">
+      <OtpInput size={4} />
+    </div>
+  );
+}
